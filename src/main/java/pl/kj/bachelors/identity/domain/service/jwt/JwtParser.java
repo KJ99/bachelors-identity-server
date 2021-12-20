6a17bdbc66218e77f97571a08ad5818189fbf65e
KJ99 <kjezierski.99@gmail.com>
2021-12-21 00:45:58 +0100

    401 when jwt expired

--- src/main/java/pl/kj/bachelors/identity/domain/service/jwt/JwtParser.java
+++ src/main/java/pl/kj/bachelors/identity/domain/service/jwt/JwtParser.java
@@ -3,7 +3,7 @@ import io.jsonwebtoken.Claims;
 import pl.kj.bachelors.identity.domain.exception.JwtInvalidException;
 
 public interface JwtParser {
-    void validateToken(String jwt) throws JwtInvalidException;
+    void validateToken(String jwt) throws Exception;
     String getUid(String jwt);
     Claims parseClaims(String jwt);
 }
